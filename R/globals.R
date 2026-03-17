c11394bdfcd6955af59f2663764f2dd5f692c88f
ielbadisy <elbadisyimad@gmail.com>
2026-03-17 00:20:51 +0000

    feat: add globals source

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+utils::globalVariables(c(
+  "value", "effect", "metric", "id", "feature", "weight",
+  "importance", "shap", "config", "yhat", "contribution", "yval",
+  "plot_value", "direction", "start", "end", "x", "y", "observation",
+  "Variable", "Importance", "Var2", "color", "from", "to", "label",
+  "i", "S", ".target", "feature.value", "interaction", "surrogate"
+))
