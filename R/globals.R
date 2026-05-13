dff8cee57bdd3fe63a05f1032378c1d21736241b
ielbadisy <elbadisyimad@gmail.com>
2026-05-13 20:38:51 +0100

    Add internal MLP learner

--- R/globals.R
+++ R/globals.R
@@ -7,5 +7,5 @@ utils::globalVariables(c(
   "surrogate", "index", "std_dev", "lower", "upper", "feature_label",
   "feature_value_scaled", "abs_shap", "feature_x", "feature_y",
   "raw_score", "config_label", "model", "conf_low", "conf_high",
-  "prob", "mean_pred", "observed", "n"
+  "prob", "mean_pred", "observed", "n", "self"
 ))
